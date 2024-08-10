82f06e865ec4086e3cbb2cfbb7edafa6f44d90d1
db318 <db318330@gmail.com>
2024-08-10 20:00:45 +1200

    Include patch for Iris Shader bug

---
+++ src/main/java/keno/backrooms_redux/mixin/IrisBridgeMixin.java
@@ -0,0 +1,16 @@
+package keno.backrooms_redux.mixin;
+
+import net.ludocrypt.limlib.impl.bridge.IrisBridge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(IrisBridge.class)
+public abstract class IrisBridgeMixin {
+    // This fixes a graphics bug that occurs when Iris Shaders is used.
+    @ModifyArg(method = "areShadersInUse", at = @At(value = "INVOKE",
+            target = "Ljava/lang/Class;forName(Ljava/lang/String;)Ljava/lang/Class;"))
+    private static String backrooms_redux$areShadersInUse(String className) {
+        return "net.irisshaders.iris.apiimpl.IrisApiV0Impl";
+    }
+}
