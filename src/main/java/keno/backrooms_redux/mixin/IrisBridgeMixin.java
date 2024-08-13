ca0ef4cbfe368f1347299312c6298835ec8f981f
db318 <db318330@gmail.com>
2024-08-13 23:52:25 +1200

    Me when I don't "remap = false"

--- src/main/java/keno/backrooms_redux/mixin/IrisBridgeMixin.java
+++ src/main/java/keno/backrooms_redux/mixin/IrisBridgeMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public abstract class IrisBridgeMixin {
     // This fixes a graphics bug that occurs when Iris Shaders is used.
     @ModifyArg(method = "areShadersInUse", at = @At(value = "INVOKE",
-            target = "Ljava/lang/Class;forName(Ljava/lang/String;)Ljava/lang/Class;"))
+            target = "Ljava/lang/Class;forName(Ljava/lang/String;)Ljava/lang/Class;"), remap = false)
     private static String backrooms_redux$areShadersInUse(String className) {
         return "net.irisshaders.iris.apiimpl.IrisApiV0Impl";
     }
